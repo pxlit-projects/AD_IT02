92de89b36ee1b1ca1c4b349078e406a2cf316e72
Leander <leander.henckens@student.pxl.be>
2015-03-23 15:45:36 +0100

    GUI hulpverlener + images

--- finah-desktop/finah-desktop/Hulpverlener.xaml.cs
+++ finah-desktop/finah-desktop/Hulpverlener.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace finah_desktop
@@ -18,7 +17,7 @@ namespace finah_desktop
     /// <summary>
     /// Interaction logic for Hulpverlener.xaml
     /// </summary>
-    public partial class Hulpverlener : UserControl
+    public partial class Hulpverlener : Window
     {
         public Hulpverlener()
         {
