09653c44f91eceeaf300be4a9ffc021f43f1f8da
Kennith Metten <kennithmetten702@hotmail.com>
2015-03-24 14:35:08 +0100

    toevoeging klasses mantelzorger/patient

---
+++ desktopapp/desktopapp/classes/Categorie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desktopapp.classes
+{
+    class Categorie
+    {
+        private String naam;
+        private String beschrijving;
+
+        public Categorie(String naam, String beschrijving)
+        {
+            this.naam = naam;
+            this.beschrijving = beschrijving;
+        }
+        
+        public String Beschrijving
+        {
+            get { return beschrijving; }
+            set { beschrijving = value; }
+        }
+
+        public String Naam
+        {
+            get { return naam; }
+            set { naam = value; }
+        }
+
+    }
+}
