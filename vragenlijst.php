2295fe28681907d9d2fcec71e93ed4b7d0fba824
GlennB1993 <11307031@student.pxl.be>
2015-03-25 19:25:56 +0100

    Update vragenlijst.php
    website voorlopige vragenlijstpagina - sprint 1

--- vragenlijst.php
+++ vragenlijst.php
@@ -1,66 +1,115 @@
 <!DOCTYPE html>
+<?php
+    /* CONNECTIE DATABASE */
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "project";
+
+    // Maak connectie
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Nagaan of connectie is gelukt
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    
+    /* DATABASE UITLEZEN EN IN ARRAY STEKEN */
+    $sql = "SELECT vraag_id, vraag_thema, vraag FROM vragenlijst";
+    $result = $conn->query($sql);
+
+    $alle_vragen = array();
+    while($row = $result->fetch_assoc()) {
+        array_push($alle_vragen, array($row["vraag_id"], $row["vraag_thema"], $row["vraag"]));
+    }
+    $conn->close();
+
+    /* TELLER INSTELLEN */
+    $teller = 0;
+    if (isset($_POST['teller'])) {
+        // TODO valideer dat teller een getal is
+        $teller = $_POST['teller'];
+    }
+    if (isset($_POST['richting'])) {
+        $richting = $_POST['richting'];
+        if ($richting === 'volgende') {
+            //1
+            $teller += 1;
+            if($teller == 53){
+                header('Location: voltooide_vragenlijst.php');
+            }
+            //2
+            // gedrukte knoppen doorsturen naar database antwoorden
+            
+        } elseif ($richting === 'vorige') {
+            $teller -= 1;
+            if ($teller == -1){
+                $teller = 0;
+        }
+    }
+    }
+?>
 <html>
 <head>
     <title>Finah - vragenlijst</title>
     <meta charset="UTF-8">
+    
     <!-- Mobiel vriendelijke viewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <!-- css file -->
     <link href="css/main.css" rel="stylesheet" type="text/css"/>
+    <link href="css/jquery-ui.css" rel="stylesheet" type="text/css"/>
+    
+    <!-- script tags -->
+    <script type="text/javascript" src="jquery/jquery.js"></script>
+    <script type="text/javascript" src="jquery/jquery-ui.js"></script>
+    
+    <script>
+        // nodig voor progressbar
+        function progress(vraag, $element) {
+            var progressBarWidth = (vraag+1)/53 * $element.width();
+            $element.find('div').animate({ width: progressBarWidth }, 500).html(vraag+1 + "/53&nbsp;");
+        }
+        
+        //nodig voor ja en nee knoppen te verbergen
+        window.onload=function(){
+            document.getElementById("toonKnoppen").style.display='none';
+        }
+    </script>  
 </head>
 <body>
-    <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "project";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "SELECT vraag_id, vraag_thema, vraag FROM vragenlijst";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-     // output data of each row
-    $alle_vragen = array();
-     while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["vraag_id"]. " - thema: ". $row["vraag_thema"]. " - vraag:" . $row["vraag"] . "<br>";
-        /*array_merge($alle_vragen, array($row["vraag_id"]=>"vraag°id",row["vraag_thema"]=>"vraag_thema",row["vraag"]=>"vraag"));*/
-        /* $row["vraag_id"] $row["vraag_thema"] $row["vraag"] */
-     }
-} else {
-     echo "0 results";
-}
-$conn->close();
-?>
-    <p><?php echo($alle_vragen[0]["vraag_thema"]);?></p>
-    <p><?php echo($alle_vragen["vraag"]);?></p>
-    <img />
+    <div id="progressbar"><div></div></div>
+    <script>progress(<?php echo $teller; ?>, $('#progressbar'));</script>
+    
+    <p><?php echo($alle_vragen[$teller][1]);?></p>
+    <p><?php echo($alle_vragen[$teller][2]);?></p>
+    <img id="foto_midden" src="image/foto/<?php echo $teller;?>.jpg"/>
     <p>Hoe ervaart u dit onderdeel?</p>
-    <button type="button">Verloopt naar wens?</button>
-    <button type="button">Probleem - niet hinderlijk?</button>
-    <button type="button">Probleem - hinderlijk voor client?</button>
-    <button type="button">Probleem - hinderlijk voor mantelzorger?</button>
-    <button type="button">Probleem - hinderlijk voor beide</button>
-    
-    
-    <p>Willen cliënt/mantelzorger dat hieraan gewerkt wordt?</p>
-    <button type="button?">Ja?</button>
-    <button type="button?">Nee?</button>
-    
-    <div id="knoppen">
-        <form action="#" class="knop_l">
-        <input type="image" src="image/vorige.png" alt="Submit" >
-        </form>
-        <p class="label_m">Pagina<?php ?>/<?php ?></p>
-        <form action="#" class="knop_r">
-        <input type="image" src="image/volgende.png" alt="Submit" >
-        </form>
-    </div>
+    <form method="POST">
+        <input type="button" value="Verloopt naar wens?" onclick="opslaan" />
+        <input type="button" value="Probleem - niet hinderlijk?" onclick="opslaan" />
+        <input type="button" value="Probleem - hinderlijk voor client?" onclick="showKnoppen()" />
+        <input type="button" value="Probleem - hinderlijk voor mantelzorger?" onclick="showKnoppen()" />
+        <input type="button" value="Probleem - hinderlijk voor beide" onclick="showKnoppen()" />
+        
+        <!-- nodig voor het tonen van ja en nee knoppen -->
+        <script>
+            function showKnoppen(){
+                document.getElementById("toonKnoppen").style.display='block';
+            }
+        </script>
+        <div id="toonKnoppen">
+            <p>Willen cliënt/mantelzorger dat hieraan gewerkt wordt?</p>
+            <input type="button" value="Ja?" />
+            <input type="button" value="Nee?" />
+        </div>
+        
+        <input type="hidden" name="teller" value="<?php echo htmlspecialchars($teller);?>" />
+        
+        <div id="knoppen">
+            <input type="submit" class="knop_l" name="richting" value="vorige" />
+            <input type="submit" class="knop_r" name="richting" value="volgende" />
+        </div>
+    </form>
 </body>
 </html>
