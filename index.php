41a6ff3a84d5f6ed98c3687eafe73f903527ff64
GlennB1993 <11307031@student.pxl.be>
2015-03-25 19:24:04 +0100

    Update index.php
    website - voorlopige index sprint1

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 <head>
     <title>Finah - inleiding</title>
     <meta charset="UTF-8">
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
     <!-- Mobiel vriendelijke viewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- css file -->
@@ -18,7 +16,7 @@
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consequat augue urna, in porta lectus ullamcorper id. Sed ultricies molestie magna, nec velit. Nam ornare nulla at finibus dictum. Ut sagittis lorem nisi, id pharetra tellus egestas eu. Vestibulum ac nisi rhoncus libero tincidunt eleifend. Nullam non enim nec quam hendrerit fringilla. Duis vitae tristique urna. Nunc porttitor nec mi ac convallis. Aliquam id vulputate nibh, ut vehicula lorem. Proin in condimentum diam, at tincidunt felis. Cras ultricies metus fringilla sollicitudin consequat. Nunc et interdum ex, sit amet scelerisque eros. Aliquam elementum ut lorem non placerat. Nulla laoreet orci nec ex sagittis dictum.         
     </div>
     <form action="vragenlijst.php">
-    <input type="image" src="image/volgende.png" alt="Submit" >
+    <input type="submit" value="volgende"/>
     </form>
 </body>
 </html>
