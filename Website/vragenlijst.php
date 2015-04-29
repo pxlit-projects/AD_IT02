c5830a06b8d680d9d59d37b21faa17fae6377b7d
GlennB1993 <11307031@student.pxl.be>
2015-04-29 18:44:02 +0200

    Update vragenlijst.php
    sprint 2

--- Website/vragenlijst.php
+++ Website/vragenlijst.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
 <?php
     /* CONNECTIE DATABASE */
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "project";
 
-    // Maak connectie
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Nagaan of connectie is gelukt
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
-    
+session_start();
+
+    if(isset($_SESSION["alle_vragen"])){
+        //
+    }else{
+    $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "project";
+
+        // Maak connectie
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Nagaan of connectie is gelukt
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
     /* DATABASE UITLEZEN EN IN ARRAY STEKEN */
     $sql = "SELECT vraag_id, vraag_thema, vraag FROM vragenlijst";
     $result = $conn->query($sql);
 
-    $alle_vragen = array();
+    $_SESSION["alle_vragen"] = array();
     while($row = $result->fetch_assoc()) {
-        array_push($alle_vragen, array($row["vraag_id"], $row["vraag_thema"], $row["vraag"]));
+        array_push($_SESSION["alle_vragen"], array($row["vraag_id"], $row["vraag_thema"], $row["vraag"]));
     }
     $conn->close();
+    }
 
     /* TELLER INSTELLEN */
     $teller = 0;
@@ -29,24 +35,48 @@
         // TODO valideer dat teller een getal is
         $teller = $_POST['teller'];
     }
+    
+    
+    if (isset($_SESSION["alle_antwoorden"])){
+        
+    }else{
+        $_SESSION["alle_antwoorden"] = new SplFixedArray(53);
+    }
+    $antwoord = "";
+    $antwoordJN = "";
     if (isset($_POST['richting'])) {
         $richting = $_POST['richting'];
-        if ($richting === 'volgende') {
-            //1
+        if ($richting === 'Volgende') {
+            //1 opslaan vraag
+            if (isset($_POST['antwoord'])){
+                $antwoord = $_POST['antwoord'];
+                $_SESSION["alle_antwoorden"][(integer)$teller] = $antwoord;
+                
+                var_dump($_SESSION["alle_antwoorden"]);
+                // nodig voor antwoord weer leeg te maken
+                $_POST['antwoord'] = null;
+                print($antwoord);
+            }/*else{
+                $leeg= "leeg";
+                var_dump($leeg);
+            }*/
             $teller += 1;
+            //2 einde vragenlijst
             if($teller == 53){
-                header('Location: voltooide_vragenlijst.php');
+                // volledige gegevens doorsturen naar database   
+                
+                var_dump($_SESSION["alle_antwoorden"]);
+                header('Location: voltooide_vragenlijst.php');               
             }
-            //2
-            // gedrukte knoppen doorsturen naar database antwoorden
             
-        } elseif ($richting === 'vorige') {
+        } elseif ($richting === 'Vorige') {
             $teller -= 1;
             if ($teller == -1){
                 $teller = 0;
+            }
         }
     }
-    }
+    
 ?>
 <html>
 <head>
@@ -78,38 +108,54 @@
     </script>  
 </head>
 <body>
-    <div id="progressbar"><div></div></div>
-    <script>progress(<?php echo $teller; ?>, $('#progressbar'));</script>
-    
-    <p><?php echo($alle_vragen[$teller][1]);?></p>
-    <p><?php echo($alle_vragen[$teller][2]);?></p>
-    <img id="foto_midden" src="image/foto/<?php echo $teller;?>.jpg"/>
-    <p>Hoe ervaart u dit onderdeel?</p>
-    <form method="POST">
-        <input type="button" value="Verloopt naar wens?" onclick="opslaan" />
-        <input type="button" value="Probleem - niet hinderlijk?" onclick="opslaan" />
-        <input type="button" value="Probleem - hinderlijk voor client?" onclick="showKnoppen()" />
-        <input type="button" value="Probleem - hinderlijk voor mantelzorger?" onclick="showKnoppen()" />
-        <input type="button" value="Probleem - hinderlijk voor beide" onclick="showKnoppen()" />
-        
-        <!-- nodig voor het tonen van ja en nee knoppen -->
-        <script>
-            function showKnoppen(){
-                document.getElementById("toonKnoppen").style.display='block';
-            }
-        </script>
-        <div id="toonKnoppen">
-            <p>Willen cliënt/mantelzorger dat hieraan gewerkt wordt?</p>
-            <input type="button" value="Ja?" />
-            <input type="button" value="Nee?" />
-        </div>
-        
-        <input type="hidden" name="teller" value="<?php echo htmlspecialchars($teller);?>" />
-        
-        <div id="knoppen">
-            <input type="submit" class="knop_l" name="richting" value="vorige" />
-            <input type="submit" class="knop_r" name="richting" value="volgende" />
-        </div>
-    </form>
+    <div id="content">
+        <div id="progressbar"><div></div></div>
+        <script>progress(<?php echo $teller; ?>, $('#progressbar'));</script>
+
+        <p><?php echo($_SESSION["alle_vragen"][$teller][1]);?></p>
+        <p><?php echo($_SESSION["alle_vragen"][$teller][2]);?></p>
+        <img id="foto_midden" src="image/foto/<?php echo $teller;?>.jpg"/>
+        <p>Hoe ervaart u dit onderdeel?</p>
+        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <button type="button" class="antwoord" value="Verloopt naar wens?" onclick="showKnoppen(0)">Verloopt naar wens?</button>
+            <button type="button" class="antwoord" value="Probleem - niet hinderlijk?" onclick="showKnoppen(1)">Probleem - niet hinderlijk?</button>
+            <button type="button" class="antwoord" value="Probleem - hinderlijk voor client?" onclick="showKnoppen(2)">Probleem - hinderlijk voor client?</button>
+            <button type="button" class="antwoord" value="Probleem - hinderlijk voor mantelzorger?" onclick="showKnoppen(3)">Probleem - hinderlijk voor mantelzorger?</button>
+            <button type="button" class="antwoord" value="Probleem - hinderlijk voor beide" onclick="showKnoppen(4)">Probleem - hinderlijk voor beide</button>
+            <!-- nodig voor het tonen van ja en nee knoppen -->
+            <script>
+                function showKnoppen($a){
+                    if($a==2 || $a==3 || $a==4){
+                        document.getElementById("toonKnoppen").style.display='block';
+                    }else{
+                        document.getElementById("toonKnoppen").style.display='none';
+                    }
+                    
+                    var x = document.getElementsByClassName("antwoord");
+                    var y = x[$a].value;
+                    document.getElementById("antwoord").value = y;
+                    
+                    var z = document.getElementsByClassName("antwoordJN");
+                    var a = z[$a].value;
+                    document.getElementById("antwoordJN").value = a;
+                    
+                }
+            </script>
+            <div id="toonKnoppen">
+                <p>Willen cliënt/mantelzorger dat hieraan gewerkt wordt?</p>
+                <button type="button" id="antwoordJN" value="Ja" >Ja?</button>
+                <button type="button" id="antwoordJN" value="Nee">Nee?</button>
+            </div>
+            <div id="knoppen">
+                <input type="submit" class="knop_l" name="richting" value="Vorige" />
+                <input type="submit" class="knop_r" name="richting" value="Volgende" />
+            </div>
+            
+            <input type="hidden" id="antwoord" name="antwoord" value="<?php echo htmlspecialchars($antwoord)?>" />
+            <input type="hidden" id="antwoordJN" name="antwoordJN" value="<?php echo htmlspecialchars($antwoordJN)?>" />
+            <input type="hidden" name="teller" value="<?php echo htmlspecialchars($teller);?>" />
+            
+        </form>
+    </div>
 </body>
 </html>
